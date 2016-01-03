4310c00f9eebb565e96d15b3ef5b7a5732077a1e
Neta Shilo <netash@post.jce.ac.il>
2016-01-04 00:35:56 +0200

    A detection test on a video

--- FinalProject/FinalProject/FinalProject.py
+++ FinalProject/FinalProject/FinalProject.py
@@ -1,3 +1,7 @@
 import Image
+from OpticFlow import optic_flow
+from Video import upload_video
 
-Image.upload_img('kids.JPG')
+#Image.upload_img('kids.JPG')
+#optic_flow()
+upload_video('shirel.mp4')
